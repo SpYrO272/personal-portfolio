47586b37fc22fc3c52d622ce4a4ff535a0c5d6d5
SpYrO272 <princeaj272@gmail.com>
2025-05-19 08:41:46 +0530

    final

--- src/components/navbar/NavbarBtn.jsx
+++ src/components/navbar/NavbarBtn.jsx
@@ -15,7 +15,7 @@ const NavbarBtn = () => {
       // </btn>
     // </Link>
     <a
-    href="../../public/images/AMAL J_MERN_DEVELOPER_RESUME.pdf" // make sure this path is correct
+    href="../../public/AMAL J_MERN_DEVELOPER_RESUME.pdf" // make sure this path is correct
     download
     className="px-4 py-2 text-xl rounded-full font-bold text-white border-cyan border flex items-center gap-1 bg-gradient-to-r from-cyan to-orange hover:border-orange hover:scale-110 transition-all duration-500 hover:shadow-cyanShadow"
   >
